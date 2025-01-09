5267a443c64d96e11f09e8a358b53e3fcde1e115
rgalim <swdevnode@gmail.com>
2025-01-09 21:35:57 +0100

    SKIW-1 update the app file

--- src/main/kotlin/skiweather/SkiWeatherApp.kt
+++ src/main/kotlin/skiweather/SkiWeatherApp.kt
@@ -1,14 +1,19 @@
 package skiweather
 
-import io.ktor.client.*
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
-import skiweather.client.httpClient
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import skiweather.client.WeatherApiClient
+import skiweather.config.httpClient
+import skiweather.config.loadConfig
 import skiweather.config.objectMapper
+import skiweather.service.LocationService
+import skiweather.service.WeatherService
 
 fun main() {
     embeddedServer(Netty, port = 8000) {
@@ -16,9 +21,22 @@ fun main() {
     }.start(wait = true)
 }
 
-fun Application.module(client: HttpClient = httpClient) {
-
+fun Application.module() {
     install(ContentNegotiation) {
         register(Json, JacksonConverter(objectMapper))
+    }
+
+    val config = loadConfig()
+    val weatherApiClient = WeatherApiClient(config, httpClient)
+    val locationService = LocationService()
+    val weatherService = WeatherService(locationService, weatherApiClient)
+
+    routing {
+        get("/weather") {
+
+            val weather = weatherService.getWeather()
+
+            call.respond(weather)
+        }
     }
 }
