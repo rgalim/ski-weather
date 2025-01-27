e72f21315f213bc2b93d0b2c728d6f10647acae0
rgalim <swdevnode@gmail.com>
2025-01-27 21:18:12 +0100

    SKIW-7 add forecast route

--- src/main/kotlin/skiweather/route/SkiAreaRoute.kt
+++ src/main/kotlin/skiweather/route/SkiAreaRoute.kt
@@ -5,7 +5,7 @@ import io.ktor.server.routing.*
 import skiweather.model.SkiAreasResponse
 import skiweather.service.SkiAreaService
 
-fun Routing.skiAreas(skiAreaService: SkiAreaService) {
+fun Routing.skiAreaRoutes(skiAreaService: SkiAreaService) {
 
     get("/api/v1/ski-area") {
         val rankedSkiAreas: SkiAreasResponse = skiAreaService.getSkiAreas()
